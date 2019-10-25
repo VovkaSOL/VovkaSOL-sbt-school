7a3eaf362b6a73106cdeaf3afbd337e3f3a44421
Vladimir S <vovkasol@mail.ru>
2019-10-25 12:30:42 +0300

    Lesson 1 strelka fix :-)

--- src/com/sbt/javaschool/rnd/lesson1args/Main.java
+++ src/com/sbt/javaschool/rnd/lesson1args/Main.java
@@ -1,14 +1,14 @@
-package com.sbt.javaschool.rnd.lesson1args;//                                                              /||\
-import com.sbt.javaschool.rnd.lesson1args.args.ArgsParser;//                                             // || \\
-//                                                                                                      //  ||  \\
-public class Main {//                                                                                  //   ||   \\
-    /*                                                                                                      ||
-                                                                                                            ||
-                                                                                                            ||
-                                                                                                            ||
-                                                                                                            ||
-                                                                                                            ||
-                                        you can edit args str "this is VovkaSOL test string" in run configuration
+package com.sbt.javaschool.rnd.lesson1args;//                                          /||\
+import com.sbt.javaschool.rnd.lesson1args.args.ArgsParser;//                         // || \\
+//                                                                                  //  ||  \\
+public class Main {//                                                              //   ||   \\
+    /*                                                                                  ||
+                                                                                        ||
+                                                                                        ||
+                                                                                        ||
+                                                                                        ||
+                                                                                        ||
+                you can edit args str "this is VovkaSOL test string" in run configuration
      */
     public static void main(String[] args) {
         ArgsParser ap= new ArgsParser();

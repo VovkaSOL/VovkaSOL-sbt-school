d09497daa8f14c012bba6d80f1134b7345fa13d4
Vladimir S <vovkasol@mail.ru>
2019-10-25 11:09:22 +0300

    lesson 1 release

---
+++ src/com/sbt/javaschool/rnd/lesson1args/Main.java
@@ -0,0 +1,21 @@
+package com.sbt.javaschool.rnd.lesson1args;//                                                        /||\
+import com.sbt.javaschool.rnd.lesson1args.args.ArgsParser;//                                       // || \\
+//                                                                                                      //  ||  \\
+public class Main {//                                                                                  //   ||   \\
+    /*                                                                                                      ||
+                                                                                                            ||
+                                                                                                            ||
+                                                                                                            ||
+                                                                                                            ||
+                                                                                                            ||
+                                        you can edit args str "this is VovkaSOL test string" in run configuration
+     */
+    public static void main(String[] args) {
+        ArgsParser ap= new ArgsParser();
+        ap.ArgsToKebabStr(args).PrintResToStdOut();
+        ap.ArgsToCamelStr(args).PrintResToStdOut();
+        ap.ArgsToSnakeStr(args).PrintResToStdOut();
+        ap.ArgsToPascalStr(args).PrintResToStdOut();
+        ap.ArgsToUpperCaseSnakeCaseStr(args).PrintResToStdOut();
+    }
+}

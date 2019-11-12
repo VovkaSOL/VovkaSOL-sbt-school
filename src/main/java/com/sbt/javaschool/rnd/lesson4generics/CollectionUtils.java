af03032ae67ea1f9a3cbbd612dbb4c872bea1e3d
Vladimir S <vovkasol@mail.ru>
2019-11-12 16:16:49 +0300

    SBS-L4 part 2 - 50 % ready, part 1-100%

--- src/main/java/com/sbt/javaschool/rnd/lesson4generics/CollectionUtils.java
+++ src/main/java/com/sbt/javaschool/rnd/lesson4generics/CollectionUtils.java
@@ -1,6 +1,8 @@
 package com.sbt.javaschool.rnd.lesson4generics;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 
 public class CollectionUtils {
@@ -8,28 +10,47 @@ public class CollectionUtils {
         destination.addAll(source);
     }
 
+    // понял что сделать с этой функцией
     public static List newArrayList() {
     }
 
-    public static int indexOf(List source, Object o) {
+    public static <T> indexOf(List source, Object o) {
+//        Iterator <T> it=source.iterator();
+//        while (it.hasNext()) {
+//            T tmp=it.next();
+//            if (tmp.equals(o)){
+//                return it.;
+//            }
+//        }
     }
 
     public static List limit(List source, int size) {
     }
 
-    public static void add(List source, Object o) {
+    public static <T> void add(List <? super T>source, T o) {
+       source.add(o);
     }
 
-    public static void removeAll(List removeFrom, List c2) {
+    public static <T> void removeAll(List <? super T> removeFrom, List <? extends T> c2) {
+
     }
 
-    public static boolean containsAll(List c1, List c2) {
+    public static <T> boolean containsAll(List <? extends T>c1, List<? extends T> c2) {
+          return c1.containsAll(c2);
     }
 
-    public static boolean containsAny(List c1, List c2) {
+    public static <T> boolean containsAny(List <? extends T>c1, List <? extends T>c2) {
+        for(T o:c2) {
+           if (c1.contains(o)) return true;
+        }
+        return false;
     }
 
-    public static List range(List list, Object min, Object max) {
+    public static <T> List<T> range(List <? extends Comparable>list, Comparable min, Comparable max) {
+       List<T> ls= new ArrayList<T>();
+       for(Comparable o:list){
+           if(o.compareTo(min))
+       }
     }
 
     public static List range(List list, Object min, Object max, Comparator comparator) {

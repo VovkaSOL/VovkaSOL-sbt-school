cbd45f7d5bb94ea87b2814b581f5b247b7eca29e
VovkaSOL <52788297+VovkaSOL@users.noreply.github.com>
2019-12-08 15:50:47 +0300

    SBS-L11 100% multithreading complete

--- src/main/java/com/sbt/javaschool/rnd/lesson11threads/Main.java
+++ src/main/java/com/sbt/javaschool/rnd/lesson11threads/Main.java
@@ -4,6 +4,35 @@ import com.sbt.javaschool.rnd.lesson11threads.sсheduler.Sсheduler;
 
 public class Main {
     public static void main(String[] args) {
-        Sсheduler sheduler=new Sсheduler(5, "log.txt");
+        //файлы берём из директории resources,
+        //в папке лежат стихи Пушкина
+        //на выходе получаем HashMap с количеством повторений слов
+        for(int i=0;i<100;i++){
+            startScheduler(5);
+        }
+
+        long minTime=100000000000000L;
+        int bestCntThreads=0;
+        for(int i=1;i<10;i++) {
+            long start = System.nanoTime();
+            startScheduler(i);
+            long time = (System.nanoTime() - start);
+            System.out.println("При "+i+" потоках время выполнения "+time/1_000_000+" мсек");
+            if (time < minTime) {
+                minTime = time;
+                bestCntThreads = i;
+            }
+        }
+
+        System.out.println("При таких входных данных, оптимальнее всего использовать "+bestCntThreads+" потоков");
+    }
+
+    private static void startScheduler(int cntThreads){
+        Sсheduler scheduler=new Sсheduler(cntThreads, new String[]{
+                "SBS-L11threads/file1.txt",
+                "SBS-L11threads/file2.txt",
+                "SBS-L11threads/file3.txt",
+                "SBS-L11threads/file4.txt",
+                "SBS-L11threads/file5.txt"});
     }
 }
--- src/main/java/com/sbt/javaschool/rnd/lesson11threads/sсheduler/Sсheduler.java
+++ src/main/java/com/sbt/javaschool/rnd/lesson11threads/sсheduler/Sсheduler.java
@@ -2,18 +2,60 @@ package com.sbt.javaschool.rnd.lesson11threads.sсheduler;
 
 import com.sbt.javaschool.rnd.lesson11threads.worker.Worker;
 
+import java.sql.SQLOutput;
+import java.util.*;
+
+import static java.lang.Thread.sleep;
+
 public class Sсheduler {
+    List<Worker> workers=new ArrayList<>();
+    Map<String,Integer> resultSetOfUnicalWordsInAllFiles = new HashMap<>(1000);
     // можем указать количество потоков для обработки списка файлов, можно указать
     //1 для последовательной однопоточной обработки
-    public Sсheduler(int cntOfThreads, String[]  fileNames) {
+    public Sсheduler(int maxCntOfThreads, String[]  fileNames) {
         int fileIdx=0;
       while(fileIdx<fileNames.length){
-        for(int thread=0;thread<cntOfThreads;thread++){
+          if(workers.size()!=0){//нужно дождаться освобождения потока если есть запущенные
+              waitUntilWorkersCompleteJob(maxCntOfThreads);
+          }
+        for(int thread=0;thread<maxCntOfThreads;thread++){
             if(fileIdx<fileNames.length){// проверяем чтобы не обратиться к несуществующему элементу массива fileNames
                 Worker worker= new Worker(fileNames[fileIdx]);//запускаем поток обработки одного файла
+                workers.add(worker);
                 fileIdx++;
             }
         }
+      }
+      //дожидаемся завершения всех workers
+      for(int w=0;w<workers.size();w++){
+          try {
+              workers.get(w).join();
+              Map<String,Integer> resMap=workers.get(w).getResults();
+              resMap.entrySet()
+                      .forEach(entry -> resultSetOfUnicalWordsInAllFiles.merge(
+                              entry.getKey(),
+                              entry.getValue(),
+                              (key, value) -> entry.getValue()   + value));
+              //resultSetOfUnicalWordsInAllFiles.addAll(workers.get(w).getResults());
+          } catch (InterruptedException e) {
+              System.out.println("Поток был прерван, но у нас никакой логики в такой ситуации, просто пропускаем");
+              e.printStackTrace();
+          }
+      }
+      //System.out.println(resultSetOfUnicalWordsInAllFiles);
     }
-    }
+  void waitUntilWorkersCompleteJob(int cntRunnedThreads) {
+      int cntRunned = 1000;
+      while (cntRunned >= cntRunnedThreads) {
+          cntRunned = 0;
+          for (Worker w : workers) {
+              if (w.isAlive()) cntRunned++;
+          }
+          try {
+              sleep(1);
+          } catch (InterruptedException e) {
+              e.printStackTrace();
+          }
+      }
+  }
 }
